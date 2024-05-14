ef42d715eb98858ae7bc7703cb1e4283aa0d7406
Jarosław Leśniak <jarek9244@gmail.com>
2024-05-14 21:43:23 +0200

    Added contract

---
+++ scripts/mint-nft.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const contract = require("../artifacts/contracts/TechnoTrove.sol/TechnoTrove.json");
+const ethers = require('ethers');
+
+// Get Alchemy API Key
+const API_KEY = process.env.API_KEY;
+
+// Define an Alchemy Provider
+const provider = new ethers.AlchemyProvider('sepolia', API_KEY)
+
+// Create a signer
+const privateKey = process.env.PRIVATE_KEY
+const signer = new ethers.Wallet(privateKey, provider)
+
+// Get contract ABI and address
+const abi = contract.abi
+const contractAddress = '0x630923461788B4366d203a17B9DBb93E638BAbA6'
+
+// Create a contract instance
+const myNftContract = new ethers.Contract(contractAddress, abi, signer)
+
+// Get the NFT Metadata IPFS URL
+const tokenUri = "https://gateway.pinata.cloud/ipfs/QmPgqV9RBprZR28iMqqL5BjviBcemZoAJGX4rkdXUXu7jJ"
+
+// Call mintNFT function
+const mintNFT = async () => {
+    let nftTxn = await myNftContract.mintNFT(signer.address, tokenUri)
+    await nftTxn.wait()
+    console.log(`NFT Minted! Check it out at: https://sepolia.etherscan.io/tx/${nftTxn.hash}`)
+}
+
+mintNFT()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
